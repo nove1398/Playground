180d579779b0c3614c4fbb47d9da57ce612c5724
Nove developer <nove1398@hotmail.com>
2021-03-04 08:59:50 -0500

    Updated notifications

---
+++ BlazorNotifications/Models/NotificationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BlazorNotifications.Models
+{
+    public class NotificationModel
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public bool Fade { get; set; }
+        public bool ShouldFade { get; set; }
+        public bool AutoClose { get; set; }
+        public bool KeepAfterRouteChange { get; set; }
+        public NotificationType Type { get; set; }
+    }
+
+    public enum NotificationType
+    {
+        Default = 1,
+        Warning = 2,
+        Error = 3,
+        Success = 4,
+        Info = 5
+    }
+}
---
+++ BlazorNotifications/Services/NotificationService.cs
@@ -0,0 +1,86 @@
+﻿using BlazorNotifications.Models;
+using System;
+
+namespace BlazorNotifications.Services
+{
+    public interface INotificationService
+    {
+        event Action<NotificationModel> OnAlert;
+
+        void Success(string message, bool keepAfterRouteChange = false, bool autoClose = true, bool fade = true);
+
+        void Error(string message, bool keepAfterRouteChange = false, bool autoClose = true, bool fade = true);
+
+        void Info(string message, bool keepAfterRouteChange = false, bool autoClose = true, bool fade = true);
+
+        void Warn(string message, bool keepAfterRouteChange = false, bool autoClose = true, bool fade = true);
+
+        void TriggerNotification(NotificationModel alert);
+
+        void ClearNotification(Guid id);
+    }
+
+    public class NotificationService : INotificationService
+    {
+        public event Action<NotificationModel> OnAlert;
+
+        public void Success(string message, bool keepAfterRouteChange = false, bool autoClose = true, bool fade = true)
+        {
+            TriggerNotification(new NotificationModel
+            {
+                Type = NotificationType.Success,
+                Text = message,
+                KeepAfterRouteChange = keepAfterRouteChange,
+                AutoClose = autoClose,
+                ShouldFade = fade
+            });
+        }
+
+        public void Error(string message, bool keepAfterRouteChange = false, bool autoClose = true, bool fade = true)
+        {
+            TriggerNotification(new NotificationModel
+            {
+                Type = NotificationType.Error,
+                Text = message,
+                KeepAfterRouteChange = keepAfterRouteChange,
+                AutoClose = autoClose,
+                ShouldFade = fade
+            });
+        }
+
+        public void Info(string message, bool keepAfterRouteChange = false, bool autoClose = true, bool fade = true)
+        {
+            TriggerNotification(new NotificationModel
+            {
+                Type = NotificationType.Info,
+                Text = message,
+                KeepAfterRouteChange = keepAfterRouteChange,
+                AutoClose = autoClose,
+                ShouldFade = fade
+            });
+        }
+
+        public void Warn(string message, bool keepAfterRouteChange = false, bool autoClose = true, bool fade = true)
+        {
+            TriggerNotification(new NotificationModel
+            {
+                Type = NotificationType.Warning,
+                Text = message,
+                KeepAfterRouteChange = keepAfterRouteChange,
+                AutoClose = autoClose,
+                ShouldFade = fade
+            });
+        }
+
+        public void TriggerNotification(NotificationModel alert)
+        {
+            alert.Id = Guid.NewGuid();
+            OnAlert?.Invoke(alert);
+        }
+
+        public void ClearNotification(Guid id)
+        {
+            OnAlert?.Invoke(new NotificationModel { Id = id });
+        }
+    }
+}
